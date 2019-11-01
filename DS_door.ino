7b26246f296597192baa5d675ba2ec043eae9ce5
Mayank bhatia <mayank.nitrate@gmail.com>
2019-11-01 14:18:49 +0530

    Update DS_door.ino

--- DS_door.ino
+++ DS_door.ino
@@ -11,9 +11,9 @@
 #include <PubSubClient.h>
 
 // your network name also called SSID
-char ssid[] = "Datasemantics";
+char ssid[] = "Enter ssid/username";
 // your network password
-char password[] = "D@ta#sem@nticz";
+char password[] = "Enter Wifi password";
 // MQTTServer to use
 char server[] = "Ur Raspberry Pi ip";
 
